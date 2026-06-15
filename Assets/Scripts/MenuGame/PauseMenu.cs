6b0fe0743ee6eba8ce5b65b322c7c7e481fc2dd1
ThinChat <spproject719@gmail.com>
2026-06-15 13:37:35 +0700

    Them Menu

---
+++ Assets/Scripts/MenuGame/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Nhấn ESC để pause
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+
+        Time.timeScale = 0f;   // Dừng game
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;   // Tiếp tục game
+        isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainScene");
+    }
+}
